221c1ed642fccc5fe179c525699a2c82d9a37075
Nijam_uddin <vu.241fa04c50@gmail.com>
2026-09-20 15:29:18 +0530

    Update prices for dum and plain biryani

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const prices = { dum: 160, plain: 120 };
+const prices = { dum: 120, plain: 100 };
 const names = { dum: "Chicken Dum Biryani", plain: "Plain Biryani Rice" };
 const cart = { dum: 0, plain: 0 };
 const form = document.getElementById("orderForm");
